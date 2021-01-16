0700b0a654387f52190b41d6b3c1153e14c5c326
biniamtekeste <biniamtekeste@github.com>
2021-01-17 01:44:39 +0300

    JavaScript basic comparison operators

---
+++ 07JavaScriptComparisonOperators/app.js
@@ -0,0 +1,10 @@
+let checkNum =900;
+if(checkNum){
+    console.log("A Number Exists")
+}
+if(checkNum>50){
+    console.log("The Number is Greater than 50")
+}
+else console.log("The number is less than 50")
+console.log(`The number is ${checkNum}`)
+
