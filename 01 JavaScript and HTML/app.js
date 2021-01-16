cdc46f597607118703333051f89278434b3d149e
biniamtekeste <biniamtekeste@github.com>
2021-01-16 18:32:57 +0300

    Initial Commit, Alert, Prompt and Var Declaration

---
+++ 01 JavaScript and HTML/app.js
@@ -0,0 +1,21 @@
+console.log("Hello World!");
+alert("Hey There, welcome!");
+var myName = "Biniam";
+var myAge = 40; //this is not my real age.
+var myCourse = "JavaScript";
+console.log(myName);
+console.log(myAge);
+console.log(myCourse);
+var a = "This is my very first javascritp program";
+console.log(a);
+
+let myMessage = false;
+if (true) {
+  let myMessage = "Hey, What are you dooing?";
+  console.log(myMessage);
+}
+console.log(myMessage);
+
+let userName = prompt("What is your name?");
+let message = "Welcome to my site";
+console.log(message + " " + userName);
