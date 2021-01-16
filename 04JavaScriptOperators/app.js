817574d38fc6b65f3d4fe6f47fb0867eba3007b2
biniamtekeste <biniamtekeste@github.com>
2021-01-16 23:53:35 +0300

    JavaScript Operators

---
+++ 04JavaScriptOperators/app.js
@@ -0,0 +1,4 @@
+console.log(`${2 + 8 + 90} This is all addition of numbers`);
+console.log(`${2 * 8 + 90} This is multiplication and addition of numbers `);
+console.log(`${25 % 3} This is module `);
+console.log(`${45 % 3} This is module `);
