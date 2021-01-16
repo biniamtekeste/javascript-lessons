7b1028e1b12ea7439d30004f05f664dc0ed8d101
biniamtekeste <biniamtekeste@github.com>
2021-01-17 01:56:17 +0300

    If else statments added

--- 07JavaScriptComparisonOperators/app.js
+++ 07JavaScriptComparisonOperators/app.js
@@ -8,3 +8,16 @@ if(checkNum>50){
 else console.log("The number is less than 50")
 console.log(`The number is ${checkNum}`)
 
+let myNum=prompt("Enter a number")
+if(myNum>10){
+    console.log("The number you enetered is greater than 10.")
+}
+else if (myNum==10){
+    console.log("The number you entered is equal to 10")
+}
+else if (myNum<10){
+    console.log ("The number you entered is less than 10")
+}
+else {
+    console.log ("You have not entered a number")
+}
