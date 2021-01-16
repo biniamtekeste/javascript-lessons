86c56b0d24f230f059b02874d947fbb3e9190dd4
biniamtekeste <biniamtekeste@github.com>
2021-01-17 00:35:52 +0300

    More on JavaScript Operators

---
+++ 05JavaScriptOperatorsMore/app.js
@@ -0,0 +1,10 @@
+myFirstNumber=8;
+mySecondNumber=4;
+myFirstNumber++;
+mySecondNumber--;
+console.log(myFirstNumber)
+console.log(mySecondNumber)
+let total = myFirstNumber+mySecondNumber;
+console.log(800+500/myFirstNumber*total-mySecondNumber)
+console.log(total++)
+console.log(total)
