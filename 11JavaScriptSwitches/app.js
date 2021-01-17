b9f599cd55428769b3462b8166b9dcc4b29557d9
biniamtekeste <biniamtekeste@github.com>
2021-01-17 18:54:55 +0300

    Names are fteched case by case using javascript swtiches.

---
+++ 11JavaScriptSwitches/app.js
@@ -0,0 +1,24 @@
+let name=prompt("Enter a name")
+switch(name){
+    case 'Biniam':
+        console.log(name +" is the best programmer in USA");
+        break;
+    
+    case "Temesghen":
+        console.log(name +" Works in Microsoft");
+        break;
+
+    case "Debas":
+        console.log(name +" is a full stack web developer");
+        break;
+
+    case "Samuel":
+        console.log (name +" is an animator");
+        break;
+
+    default:
+        console.log(`We're sorry, the name ${name} is not found on our database.`);
+        break;
+
+
+}
