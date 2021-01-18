3b0caf33fac42f68f272128bf4c89b7dc0153342
biniamtekeste <biniamtekeste@github.com>
2021-01-18 23:52:22 +0300

    Add a function that returns in two different ways

---
+++ 14JavaScriptFunctionThatReturns/app.js
@@ -0,0 +1,14 @@
+console.log(multiplier(10,39));
+
+function multiplier(x,y){
+    return x*y;
+}
+
+//another way of declaring a function
+
+let multiplier2= function (a,b){
+    let temp = a*b;
+    return temp;
+}
+
+console.log(multiplier2(10,39));
