0eb0fabe2dd932aac1e5e457c1fea1ca3644c44d
biniamtekeste <biniamtekeste@github.com>
2021-01-18 19:33:49 +0300

    addTen function is added

--- 12JavaScriptFunctions/app.js
+++ 12JavaScriptFunctions/app.js
@@ -1,21 +1,23 @@
-let outPut="Hello World";
-let counter=0;
+let outPut = "Hello World";
+let counter = 0;
 welcome(outPut);
 
-function welcome(para){
-    counter++;
-    let temp = counter + " times run";
+function welcome(para) {
+  counter++;
+  let temp = counter + " times run";
 
-    console.log(para);
-    console.log(temp);
-
+  console.log(para);
+  console.log(temp);
 }
 // A Function that adds 10 to any number passed to it.
 
-let myNum =50;
+let myNum = 50;
 addTen(myNum);
 
-function addTen(num){
-    let temp =Number(num);
-    console.log (temp+10);
+function addTen(num) {
+  let temp = Number(num);
+  console.log(temp + 10);
 }
+
+welcome(outPut);
+addTen(450);
