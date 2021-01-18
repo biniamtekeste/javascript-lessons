f53b3d301d39692b0475504fb40661dfbb0fad82
biniamtekeste <biniamtekeste@github.com>
2021-01-19 02:10:42 +0300

    Function Declaration Vs Function Expression explained

---
+++ 17JavaScriptFunctionDeclartionVsFunctionExpression/app.js
@@ -0,0 +1,15 @@
+// Function Expression
+
+let test1=function(num){
+    return num*num;
+}
+console.log(test1(4))
+
+console.log(test1(5))
+console.log(test2(5))
+
+function test2(num){
+    return num*num;
+}
+
+console.log(test2(4))
