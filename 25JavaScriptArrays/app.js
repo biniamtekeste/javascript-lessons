e4cec0597ed7db5c386fdbcd49a3978020952bbf
biniamtekeste <biniamtekeste@github.com>
2021-01-20 18:54:06 +0300

    display the sub elements of the listArray

--- 25JavaScriptArrays/app.js
+++ 25JavaScriptArrays/app.js
@@ -6,4 +6,6 @@ console.log(listArray[0]);
 let a=listArray.length;
 for(let i=0; i<=a-1; i++)
 console.log(listArray[i])
+// Displaying the elements of the sub-elements of the listArray
 console.log(listArray[7].color)
+console.log(listArray[6][0])
