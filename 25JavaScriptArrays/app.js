a63e3ff0a08952dbc924b2a3e503a7587849de44
biniamtekeste <biniamtekeste@github.com>
2021-01-20 18:50:54 +0300

    create an array of different values and display its elements

---
+++ 25JavaScriptArrays/app.js
@@ -0,0 +1,9 @@
+const listArray=["Bini", 35, "null", "undefined","ture", "false",
+["Asmara", "Eritrea", 14, ],
+{test:'one', color:'pink'}];
+console.log(listArray[0]);
+
+let a=listArray.length;
+for(let i=0; i<=a-1; i++)
+console.log(listArray[i])
+console.log(listArray[7].color)
