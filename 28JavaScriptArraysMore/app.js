4314e6e497be3b09169a026ecf06bdfa278caea0
biniamtekeste <biniamtekeste@github.com>
2021-01-24 22:20:26 +0300

    filter the numbers with the array filter method

--- 28JavaScriptArraysMore/app.js
+++ 28JavaScriptArraysMore/app.js
@@ -1,5 +1,5 @@
 const array1=["a","B","S",-8,"T",8,23,]
-const array2=[1,8,32,2,67,0,17,9,66,91,56,10,-8,71,]
+const array2=[1,8,32,2,67,0,17,9,66,91,56,10,-8,71]
 let num;
 console.log(array1)
 console.log(array2)
@@ -12,21 +12,28 @@ console.log(array1.indexOf("10"))//returns -1, coz 10 doesn't exist
 console.log(num)
 let sorted1=array1.sort() //Sorts descending order
 console.log(sorted1)
-let sorted2=array2.sort() //Sorts descending order
-console.log(sorted2)
+// let sorted2=array2.sort() //Sorts descending order
+// console.log(sorted2)
 
 console.log(array1.length)
 console.log(array2.length)
 
 // Let's reverse the arrays
 console.log(array1.reverse())
-console.log(array2.reverse())
+// console.log(array2.reverse())
 
 // Let's find somethng from the arrays
 let found=array1.find(function (n){
     return n===67;
 })
 console.log(found)
+
+let found1=array2.find(function (n){
+    console.log(n);
+    return n>90
+})
+console.log(found1)
+console.log("above me")
 
 // Let's concatinate the two arrays
 const newArray=array1.concat(array2)
---
+++ 29JavaScriptArrayFilter/app.js
@@ -0,0 +1,7 @@
+const numbers=[77, 44, 2, 162, 18, 244, 71]
+
+let result=numbers.filter(function (num){
+    return num>75;
+});
+
+console.log(result);
