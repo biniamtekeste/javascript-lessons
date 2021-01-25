996cdf05972c5d39a5124ae63fe40ae8a750acea
biniamtekeste <biniamtekeste@github.com>
2021-01-25 23:29:15 +0300

    array filter applied

--- 29JavaScriptArrayFilter/app.js
+++ 29JavaScriptArrayFilter/app.js
@@ -10,3 +10,9 @@ const words=["Biniam", "Temie","Tekeste Bahta", "Hanibal", "Fruit", "Aki", "Abri
 
 result=words.filter(word => word.length>6);
 console.log(result);
+
+const mixed=[12, -2, 4, 0, -23, 33, -34, 8, 90, -1, -100, 456]
+
+result=mixed.filter(n=> n<60);
+console.log(result);
+
