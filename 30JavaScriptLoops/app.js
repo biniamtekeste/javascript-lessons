025e4a4d336b1a9791535aadcafda263fbbda329
biniamtekeste <biniamtekeste@github.com>
2021-01-25 23:48:28 +0300

    Do While loop

--- 30JavaScriptLoops/app.js
+++ 30JavaScriptLoops/app.js
@@ -1,5 +1,11 @@
 let x=0
 while(x<10){
-    console.log(x);
     x++;
+    console.log('While loop count at '+x);
 }
+
+let i=0;
+do{
+    i++
+    console.log('Do While loop count at '+i)
+}while(i<7);
