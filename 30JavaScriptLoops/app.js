e0d3434b44f00f01ba47490cb60a39d8ddd48108
biniamtekeste <biniamtekeste@github.com>
2021-01-25 23:59:15 +0300

    For Loop

--- 30JavaScriptLoops/app.js
+++ 30JavaScriptLoops/app.js
@@ -9,3 +9,20 @@ do{
     i++
     console.log('Do While loop count at '+i)
 }while(i<7);
+
+// let y=10;
+// while(y<5){
+//     y++;
+//     console.log('While loop count at '+y) //Will run once even if condition is not true
+// }
+
+let z=10;
+do{
+    z++;
+    console.log('Do While Loop count at '+z)
+}while(z<5);
+
+for (let c=0; c<19;c++){
+
+    console.log('for loop count at '+c)
+}
