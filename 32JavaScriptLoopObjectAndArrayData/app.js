ba80d6d5a552d18b2f0149c17d777aa703b9a581
biniamtekeste <biniamtekeste@github.com>
2021-01-28 01:01:16 +0300

    loop opbject and array data

---
+++ 32JavaScriptLoopObjectAndArrayData/app.js
@@ -0,0 +1,10 @@
+const array1=['a','b','c'];
+for (let w=0;w<array1.length;w++){
+    console.log(array1[w]);
+}
+
+// array1.forEach(n=>console.log(n))
+
+array1.forEach(function (element, index, array) {
+    console.log(element);
+})
